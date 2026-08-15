f8a7aed51542e68448d032a86e4f81647ad05bf6
Bajgude1999 <119029162+Bajgude1999@users.noreply.github.com>
2026-08-15 16:58:48 +0530

    Update environment.ts

--- src/environments/environment/environment.ts
+++ src/environments/environment/environment.ts
@@ -2,9 +2,9 @@
 export const environment = {
   production: false,
 //  apiBaseUrl: 'http://10.190.143.90/agrostar',
-   apiBaseUrl: 'https://goat-thesaurus-aspects-yield.trycloudflare.com/agrostar',
+   apiBaseUrl: 'https://accessory-author-cargo-rising.trycloudflare.com/agrostar',
  //  websocketUrl: 'http://10.190.143.90/agrostar',
-websocketUrl: 'https://goat-thesaurus-aspects-yield.trycloudflare.com/agrostar',
+websocketUrl: 'https://accessory-author-cargo-rising.trycloudflare.com/agrostar',
   defaultLanguage: 'en',
 encriptionKey: 'abcd@1234567890xyz123ABCD@123456',
   supportedLanguages: [
