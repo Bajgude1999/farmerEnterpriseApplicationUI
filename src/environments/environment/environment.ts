8c23a5e707c3cedf2b0f309cd5ff01d420125954
Bajgude1999 <119029162+Bajgude1999@users.noreply.github.com>
2026-08-15 16:54:35 +0530

    Update environment.ts
    checking clouldflare backend is connect or not

--- src/environments/environment/environment.ts
+++ src/environments/environment/environment.ts
@@ -1,10 +1,10 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://10.190.143.90/agrostar',
- //apiBaseUrl: 'https://lights-tiles-terminals-heel.trycloudflare.com/agrostar',
-   websocketUrl: 'http://10.190.143.90/agrostar',
-
+//  apiBaseUrl: 'http://10.190.143.90/agrostar',
+   apiBaseUrl: 'https://goat-thesaurus-aspects-yield.trycloudflare.com/agrostar',
+ //  websocketUrl: 'http://10.190.143.90/agrostar',
+websocketUrl: 'https://goat-thesaurus-aspects-yield.trycloudflare.com/agrostar',
   defaultLanguage: 'en',
 encriptionKey: 'abcd@1234567890xyz123ABCD@123456',
   supportedLanguages: [
