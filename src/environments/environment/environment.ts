d9dd956251abda53cb6b660914e5adbfc2b711e1
Bajgude1999 <119029162+Bajgude1999@users.noreply.github.com>
2026-08-17 21:47:05 +0530

    Update API and WebSocket URLs in environment.ts

--- src/environments/environment/environment.ts
+++ src/environments/environment/environment.ts
@@ -2,9 +2,9 @@
 export const environment = {
   production: false,
 //  apiBaseUrl: 'http://10.190.143.90/agrostar',
-   apiBaseUrl: 'https://photographers-accommodate-preview-ballot.trycloudflare.com/agrostar',
+   apiBaseUrl: 'https://preventing-original-voted-filling.trycloudflare.com/agrostar',
  //  websocketUrl: 'http://10.190.143.90/agrostar',
-websocketUrl: 'https://photographers-accommodate-preview-ballot.trycloudflare.com/agrostar',
+websocketUrl: 'https://preventing-original-voted-filling.trycloudflare.com/agrostar',
   defaultLanguage: 'en',
 encriptionKey: 'abcd@1234567890xyz123ABCD@123456',
   supportedLanguages: [
