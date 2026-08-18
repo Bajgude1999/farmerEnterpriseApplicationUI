22f16f301d972cf143fc8b7066d3cd2b34564c7c
yogesh.bajgude <yogesh.bajgude@epps-erp.in>
2026-08-18 21:45:02 +0530

    url change2

--- src/environments/environment/environment.ts
+++ src/environments/environment/environment.ts
@@ -2,9 +2,9 @@
 export const environment = {
   production: false,
 //  apiBaseUrl: 'http://10.190.143.90/agrostar',
-   apiBaseUrl: 'https://pit-distinction-conservation-supports.trycloudflare.com/agrostar',
+   apiBaseUrl: 'https://jan-phillips-paragraph-abc.trycloudflare.com/agrostar',
  //  websocketUrl: 'http://10.190.143.90/agrostar',
-websocketUrl: 'https://pit-distinction-conservation-supports.trycloudflare.com/agrostar',
+websocketUrl: 'https://jan-phillips-paragraph-abc.trycloudflare.com/agrostar',
   defaultLanguage: 'en',
 encriptionKey: 'abcd@1234567890xyz123ABCD@123456',
   supportedLanguages: [
